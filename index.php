e8f0eb0f8e208a8c008c5511839f46c8c71088ae
robpap96 <robertopapagni1996@outlook.it>
2022-12-14 16:15:23 +0100

    prova bonus non riuscita

--- index.php
+++ index.php
@@ -40,6 +40,7 @@
 
     ];
 
+    $park_flag = isset($_GET['parking'])=='Si' ? true : false;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -53,36 +54,49 @@
         <!-- ///BOOTSTRAP -->
     </head>
     <body>
-        
-        <!-- TAB-BOOTSTRAP -->
-        <table class="table table-striped">
-            <thead>
-        
-            </thead>
-            <tbody>
-                
-                <tr>
-                    <th scope="row">Name</th>
-                    <th scope="row">Description</th>
-                    <th scope="row">Parking</th>
-                    <th scope="row">Vote</th>
-                    <th scope="row">Distance to Center</th>
+        <div class="container py-3 px-5">
+            <form action="index.php" method="GET">
+                <label for="park_flag">Selezioni se desidera o meno il parcheggio</label>
+                <select name="parking" id="park_flag" class="form-select " aria-label="Default select example">
+                    <option selected>Scegliere filtro parcheggio</option>
+                    <option value="Si">Si</option>
+                    <option value="No">No</option>
+                </select>
+                <button type="submit">Filtra</button>
+            </form>
 
-                </tr>
-                <?php foreach($hotels as $hotel){ ?> 
+        </div>
+        <div class="container py-3 px-5">
+            <!-- TAB-BOOTSTRAP -->
+            <table class="table table-striped">
+                <thead>
+            
+                </thead>
+                <tbody>       
                     <tr>
-                        <td><?php echo $hotel["name"]?></td>           
-                        <td><?php echo $hotel["description"]?></td>
-                        <td><?php echo $hotel["parking"] ? 'si' : 'no' ?></td>
-                        <td><?php echo $hotel["vote"]?></td>
-                        <td><?php echo $hotel["distance_to_center"]?></td>
+                        <th scope="row">Name</th>
+                        <th scope="row">Description</th>
+                        <th scope="row">Parking</th>
+                        <th scope="row">Vote</th>
+                        <th scope="row">Distance to Center</th>
                     </tr>
-                
-                <?php } ?>
-
-            </tbody>
-        </table>
-        <!-- ///TAB-BOOTSTRAP -->
+                    <?php foreach($hotels as $hotel){ 
+                        if($park_flag){    
+                    ?> 
+                        <tr>
+                            <td><?php echo $hotel["name"]?></td>           
+                            <td><?php echo $hotel["description"]?></td>
+                            <td><?php echo $hotel["parking"] ? 'si' : 'no' ?></td>
+                            <td><?php echo $hotel["vote"]?></td>
+                            <td><?php echo $hotel["distance_to_center"]?></td>
+                        </tr>                 
+                    <?php 
+                        }
+                    } ?>
+                </tbody>
+            </table>
+            <!-- ///TAB-BOOTSTRAP -->
+        </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
 </html>
