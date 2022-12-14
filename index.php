bc3226daabf85f525f0c3f6d1c2bf9ea47a1b5d8
robpap96 <robertopapagni1996@outlook.it>
2022-12-14 14:13:16 +0100

    tab aggiunta

--- index.php
+++ index.php
@@ -58,6 +58,26 @@
                 echo $hotel["name"];
         ?> 
 
+        <!-- TAB-BOOTSTRAP -->
+        <table class="table table-striped">
+            <thead>
+        
+            </thead>
+            <tbody>
+                <tr class="table-active">
+                ...
+                </tr>
+                <tr>
+                ...
+                </tr>
+                <tr>
+                <th scope="row">3</th>
+                <td colspan="2" class="table-active">Larry the Bird</td>
+                <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>
+        <!-- ///TAB-BOOTSTRAP -->
         
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
